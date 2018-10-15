74f74fc28c668fd4118575fdb50add0c9162e52b
moisesmcardona <moises@moises-studios.com>
2018-10-15 09:45:52 -0400

    Add project files.

---
+++ Simple Video Rotator/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SaveFileDialog2 = New System.Windows.Forms.SaveFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(351, 65)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(204, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Step 1: Select a video to change rotation:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 103)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(261, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(282, 101)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(282, 142)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 144)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(261, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(249, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Step 2: Choose a destination place for the fixed file:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 194)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(253, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Step 3: Enter a rotation in degrees to alter the video:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(60, 211)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(64, 20)
+        Me.TextBox3.TabIndex = 8
+        Me.TextBox3.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 238)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(150, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Step 4: Start! or Add to Queue"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 254)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(147, 40)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Apply Rotation"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(571, 297)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(121, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "v1.0 by Moises Cardona"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(395, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(292, 240)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Queue"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(6, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(279, 212)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(525, 258)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(79, 36)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(165, 254)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(147, 40)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Add to Queue"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(395, 258)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(124, 36)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "Apply Rotation to items"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(610, 258)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(77, 36)
+        Me.Button7.TabIndex = 17
+        Me.Button7.Text = "Clear"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(15, 210)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(39, 20)
+        Me.Button8.TabIndex = 19
+        Me.Button8.Text = "-90"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(130, 211)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(39, 20)
+        Me.Button9.TabIndex = 20
+        Me.Button9.Text = "+90"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(15, 170)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(283, 17)
+        Me.CheckBox1.TabIndex = 21
+        Me.CheckBox1.Text = "Store fixed files in folder instead of specifying filenames"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 318)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Simple Video Rotator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents SaveFileDialog2 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+
+End Class
---
+++ Simple Video Rotator/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Net
+Imports Microsoft.Win32
+
+Public Class Form1
+    Dim InputFileList As New ListBox
+    Dim OutputFileList As New ListBox
+    Dim RotationParameters As New ListBox
+    Dim CurrInputFile As String
+    Dim CurrOutputFile As String
+    Dim CurrRotation As Integer
+    Public Sub ProcessStart(ByRef Executable, ByRef CommandLine)
+        Dim P As New Process
+        With P
+            .StartInfo.FileName = Executable
+            .StartInfo.Arguments = CommandLine
+            .StartInfo.RedirectStandardOutput = False
+            .StartInfo.RedirectStandardError = False
+            .StartInfo.UseShellExecute = True
+            .StartInfo.CreateNoWindow = False
+            .Start()
+            .WaitForExit()
+        End With
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text = "" Then
+            MsgBox("An Input video is needed in order to process the rotation")
+        ElseIf TextBox2.Text = "" Then
+            MsgBox("An Output location is needed in order to process the rotation")
+        ElseIf TextBox3.Text = "" Then
+            MsgBox("Rotation value is not valid. It must be a number between 0 and 360")
+        ElseIf TextBox3.Text < 0 Or TextBox3.Text > 360 Then
+            MsgBox("Rotation value is not valid. It must be a number between 0 and 360")
+        Else
+            CurrInputFile = TextBox1.Text
+            CurrOutputFile = TextBox2.Text
+            CurrRotation = TextBox3.Text
+            If CheckBox1.Checked = False Then
+                ProcessStart("ffmpeg.exe", "-i """ & TextBox1.Text & """ -c copy -metadata:s:v:0 rotate=" & TextBox3.Text & " """ & TextBox2.Text & """")
+            Else
+                ProcessStart("ffmpeg.exe", "-i """ & TextBox1.Text & """ -c copy -metadata:s:v:0 rotate=" & TextBox3.Text & " """ & TextBox2.Text & "\" & My.Computer.FileSystem.GetName(TextBox1.Text) & """")
+            End If
+            MsgBox("Video processed!")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" = False Then OpenFileDialog1.FileName = My.Computer.FileSystem.GetName(TextBox1.Text) Else OpenFileDialog1.FileName = ""
+        OpenFileDialog1.Filter = "MP4 Video (*.mp4)|*.mp4|Apple iOS device video (*.mov)|*.mov|All file types|*.*"
+        OpenFileDialog1.Title = "Select a source video to process"
+        Dim DialogOk As DialogResult = OpenFileDialog1.ShowDialog
+        If DialogOk = Windows.Forms.DialogResult.OK Then If OpenFileDialog1.FileName = "" = False Then TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If CheckBox1.Checked = False Then
+            If TextBox2.Text = "" = False Then SaveFileDialog2.FileName = My.Computer.FileSystem.GetName(TextBox2.Text) Else SaveFileDialog2.FileName = ""
+            SaveFileDialog2.Filter = "MP4 Video (*.mp4)|*.mp4|Apple iOS device video (*.mov)|*.mov|All file types|*.*"
+            SaveFileDialog2.Title = "Select a destination place and type a filename"
+            Dim DialogOk As DialogResult = SaveFileDialog2.ShowDialog
+            If DialogOk = Windows.Forms.DialogResult.OK Then If SaveFileDialog2.FileName = "" = False Then TextBox2.Text = SaveFileDialog2.FileName
+        Else
+            FolderBrowserDialog1.ShowNewFolderButton = True
+            Dim DialogOk As DialogResult = FolderBrowserDialog1.ShowDialog
+            If DialogOk = Windows.Forms.DialogResult.OK Then If FolderBrowserDialog1.SelectedPath = "" = False Then TextBox2.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text = "" Then
+            MsgBox("An Input video is needed in order to process the rotation")
+        ElseIf TextBox2.Text = "" Then
+            MsgBox("An Output location is needed in order to process the rotation")
+        ElseIf TextBox3.Text = "" Then
+            MsgBox("Rotation value is not valid. It must be a number between 0 and 360")
+        ElseIf TextBox3.Text < 0 Or TextBox3.Text > 360 Then
+            MsgBox("Rotation value is not valid. It must be a number between 0 and 360")
+        Else
+            ListBox1.Items.Add(My.Computer.FileSystem.GetName(TextBox1.Text))
+            InputFileList.Items.Add(TextBox1.Text)
+            If CheckBox1.Checked = False Then OutputFileList.Items.Add(TextBox2.Text) Else OutputFileList.Items.Add(TextBox2.Text & "\" & My.Computer.FileSystem.GetName(TextBox1.Text))
+            RotationParameters.Items.Add(TextBox3.Text)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim Index As Integer = ListBox1.SelectedIndex
+        ListBox1.Items.RemoveAt(Index)
+        InputFileList.Items.RemoveAt(Index)
+        OutputFileList.Items.RemoveAt(Index)
+        RotationParameters.Items.RemoveAt(Index)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedItem = "" = False Then Button4.Enabled = True Else Button4.Enabled = False
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim processeditems As Integer = 0
+        For i As Integer = 0 To ListBox1.Items.Count - 1
+            CurrInputFile = InputFileList.Items.Item(i)
+            CurrOutputFile = OutputFileList.Items.Item(i)
+            CurrRotation = RotationParameters.Items.Item(i)
+            ProcessStart("ffmpeg.exe", "-i """ & CurrInputFile & """ -c copy -metadata:s:v:0 rotate=" & CurrRotation & " """ & CurrOutputFile & """")
+            processeditems = processeditems + 1
+        Next
+        MsgBox(processeditems & " Videos processed!")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        ListBox1.Items.Clear()
+        InputFileList.Items.Clear()
+        OutputFileList.Items.Clear()
+        RotationParameters.Items.Clear()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If IsNumeric(TextBox3.Text) Then
+            If TextBox3.Text < 90 = False Then
+                TextBox3.Text = TextBox3.Text - 90
+            Else
+                TextBox3.Text = 0
+            End If
+        Else : TextBox3.Text = 0
+        End If
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If IsNumeric(TextBox3.Text) Then
+            If TextBox3.Text > 270 = False Then
+                TextBox3.Text = TextBox3.Text + 90
+            Else
+                TextBox3.Text = 360
+            End If
+        Else : TextBox3.Text = 90
+        End If
+    End Sub
+
+End Class
